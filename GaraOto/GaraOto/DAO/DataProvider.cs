4f5eceb285c655906fc8db1c0bffca445116858c
MINHTIEN\Minh Tien <vanminhtien96@gmail.com>
2017-12-25 23:51:02 +0700

    Add dataprovider use database QLGR

---
+++ GaraOto/GaraOto/DAO/DataProvider.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaraOto.DAO
+{
+    class DataProvider
+    {
+        private static DataProvider instance;
+        //Insert data connection from sql to connectionSTR;
+        private string connectionSTR = @"Data Source=MINHTIEN\SQLEXPRESS;Initial Catalog=QLGR;Integrated Security=True";
+
+        internal static DataProvider Instance
+        {
+            get
+            {
+                return instance;
+            }
+
+            set
+            {
+                instance = value;
+            }
+        }
+
+        private DataProvider() { }
+
+        public DataTable ExecuteQuery(string query, object[] paramater = null)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (paramater != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, paramater[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                adapter.Fill(data);
+
+                connection.Close();
+            };
+
+            return data;
+        }
+
+        public int ExecuteNonQuery(string query, object[] paramater = null)
+        {
+            int data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (paramater != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, paramater[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = command.ExecuteNonQuery();
+
+                connection.Close();
+            };
+
+            return data;
+        }
+
+        public object ExecuteScalar(string query, object[] paramater = null)
+        {
+            object data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (paramater != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, paramater[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = command.ExecuteScalar();
+
+                connection.Close();
+            };
+
+            return data;
+        }
+    }
+}
