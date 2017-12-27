215315a786417f175fc58b7dd02e3d2850ed27ed
MINHTIEN\Minh Tien <vanminhtien96@gmail.com>
2017-12-28 01:34:53 +0700

    add fHieuXe

---
+++ GaraOto/GaraOto/fHieuXe.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace GaraOto
+{
+    partial class fHieuXe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvHieuXe = new System.Windows.Forms.DataGridView();
+            this.tbHieuXe = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.HIEUXE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHieuXe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvHieuXe
+            // 
+            this.dgvHieuXe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHieuXe.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.HIEUXE});
+            this.dgvHieuXe.Location = new System.Drawing.Point(30, 60);
+            this.dgvHieuXe.Name = "dgvHieuXe";
+            this.dgvHieuXe.Size = new System.Drawing.Size(192, 174);
+            this.dgvHieuXe.TabIndex = 0;
+            // 
+            // tbHieuXe
+            // 
+            this.tbHieuXe.Enabled = false;
+            this.tbHieuXe.Location = new System.Drawing.Point(228, 60);
+            this.tbHieuXe.Name = "tbHieuXe";
+            this.tbHieuXe.Size = new System.Drawing.Size(157, 20);
+            this.tbHieuXe.TabIndex = 1;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(228, 92);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(156, 31);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(228, 129);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(156, 31);
+            this.btnXoa.TabIndex = 3;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Enabled = false;
+            this.btnLuu.Location = new System.Drawing.Point(228, 166);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(156, 31);
+            this.btnLuu.TabIndex = 4;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Enabled = false;
+            this.btnHuy.Location = new System.Drawing.Point(229, 203);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(156, 31);
+            this.btnHuy.TabIndex = 5;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(139, 254);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(131, 26);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Danh sách hiệu xe";
+            // 
+            // HIEUXE
+            // 
+            this.HIEUXE.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.HIEUXE.DataPropertyName = "HIEUXE";
+            this.HIEUXE.HeaderText = "Hiệu Xe";
+            this.HIEUXE.Name = "HIEUXE";
+            this.HIEUXE.ReadOnly = true;
+            // 
+            // fHieuXe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 301);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.tbHieuXe);
+            this.Controls.Add(this.dgvHieuXe);
+            this.Name = "fHieuXe";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hiệu Xe";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHieuXe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvHieuXe;
+        private System.Windows.Forms.TextBox tbHieuXe;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HIEUXE;
+    }
+}
---
+++ GaraOto/GaraOto/fHieuXe.cs
@@ -0,0 +1,96 @@
+﻿using GaraOto.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GaraOto
+{
+    public partial class fHieuXe : Form
+    {
+        BindingSource listHieuXe = new BindingSource();
+        public fHieuXe()
+        {
+            InitializeComponent();
+            Load();
+        }
+
+        public new void Load()
+        {
+            dgvHieuXe.DataSource = listHieuXe;
+            listHieuXe.DataSource = HieuXeDAO.Instance.getListHieuXe();
+            tbHieuXe.DataBindings.Add(new Binding("Text", dgvHieuXe.DataSource, "HIEUXE"));
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            tbHieuXe.Clear();
+            tbHieuXe.Enabled = true;
+
+            btnHuy.Enabled = true;
+            btnLuu.Enabled = true;
+
+            btnThem.Enabled = false;
+            btnXoa.Enabled = false;
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {            
+            if (MessageBox.Show("Bạn chắc chắn muốn xóa hiệu xe " + tbHieuXe.Text + "?", "Thông báo", MessageBoxButtons.OKCancel)
+                == System.Windows.Forms.DialogResult.OK) {
+                HieuXeDAO.Instance.delHieuXe(tbHieuXe.Text);
+                listHieuXe.DataSource = HieuXeDAO.Instance.getListHieuXe();
+            };            
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {            
+            tbHieuXe.Enabled = false;
+
+            btnHuy.Enabled = false;
+            btnLuu.Enabled = false;
+            
+            btnThem.Enabled = true;
+            btnXoa.Enabled = true;
+
+            if (tbHieuXe.Text != "")
+            {
+                DataTable numrow = HieuXeDAO.Instance.addHieuXe(tbHieuXe.Text);
+                int i = (int) numrow.Rows[0].ItemArray[0];
+                if (i == 0)
+                {
+                    MessageBox.Show("Thêm hiệu xe thành công");
+                }
+                else
+                {
+                    MessageBox.Show("Thêm hiệu xe đã tồn tại");
+                }           
+            }else
+            {
+                MessageBox.Show("Hiệu xe không để trống");
+            }
+            listHieuXe.DataSource = HieuXeDAO.Instance.getListHieuXe();
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            tbHieuXe.Enabled = false;
+
+            btnHuy.Enabled = false;
+            btnLuu.Enabled = false;            
+
+            btnThem.Enabled = true;
+            btnXoa.Enabled = true;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
